8e46e3537dbf68e5124099682e02d35ccc8fb227
kevin.juguin <kevin.juguin@epsi.fr>
2021-03-11 14:30:35 +0100

    first test

---
+++ src/test/java/com/epsi/mycal/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.epsi.mycal;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		int a = 2;
+		int b = 3;
+		Calculator calculator = new Calculator();
+		int actual = calculator.add(a, b);
+		assertEquals(a + b, actual);
+	}
+
+	@Test
+	public void testSub() {
+		int a = 2;
+		int b = 3;
+		Calculator calculator = new Calculator();
+		int actual = calculator.sub(a, b);
+		assertEquals(a - b, actual);
+	}
+
+}
